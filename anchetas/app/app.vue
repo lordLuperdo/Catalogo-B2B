24b240c256b29b3f160e0230a4a67e1d21a9060c
Design MacBook <coneoandres18@gmail.com>
2025-11-26 17:20:36 -0500

    cambio de fuente2

--- anchetas/app/app.vue
+++ anchetas/app/app.vue
@@ -6,7 +6,7 @@
 <style>
 @font-face {
   font-family: "Pincoya";
-  src: url("/fonts/PincoyaBlack.otf") format("opentype");
+  src: url("../public/fonts/CHRISTMASCOMEBACK.otf") format("opentype");
   font-weight: normal;
   font-style: normal;
   font-display: swap;
